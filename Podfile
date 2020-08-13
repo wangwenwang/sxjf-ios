a6d0660d2b438d6eec1a9155e069e33912092696
wangww <372266373@qq.com>
2020-08-13 16:45:21 +0800

    第一次提交

---
+++ Podfile
@@ -0,0 +1,13 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+
+target 'tms-ios' do
+pod 'SSZipArchive'
+pod 'MBProgressHUD', '~> 1.0.0'
+pod 'AFNetworking', '~> 3.0'
+pod 'BaiduMapKit'
+pod 'SGQRCode', '~> 3.0.1'
+pod 'LBXScan/LBXZXing','~> 2.3'
+pod 'LBXScan/UI','~> 2.3'
+pod 'LBXScan/LBXNative','~> 2.3'
+end
---
+++ tms-ios/ViewController.h
@@ -0,0 +1,26 @@
+//
+//  ViewController.h
+//  tms-ios
+//
+//  Created by wenwang wang on 2018/9/10.
+//  Copyright © 2018年 wenwang wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <ZipArchive.h>
+#import <JavaScriptCore/JavaScriptCore.h>
+#import "Tools.h"
+#import <MapKit/MapKit.h>
+#import "ServiceTools.h"
+#import "AppDelegate.h"
+#import "CheckOrderPathViewController.h"
+#import "IOSToVue.h"
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) WKWebView *webView;
+
+- (void)addWebView;
+
+@end
+
